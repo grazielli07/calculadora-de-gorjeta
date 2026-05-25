cedf2623e8a0f684db96d8494d7c794d65c2163e
Grazielli Proenca <grazilivia07@gmail.com>
2026-05-25 00:24:54 -0300

    arrumando os últimos erros

--- script.js
+++ script.js
@@ -56,10 +56,10 @@ gorjetaInput.addEventListener("input", receberPorcentagem)
 function calcular() {
     if(conta !== 0 && porcentagem !== 0 && pessoas !== 0) {
       const strongGorjetaTotal = document.querySelector(".gorjeta-total > strong")
-      strongGorjetaTotal.innerHTML = `R$ ${(conta * gorjeta / pessoas).toFixed(2)}`
+      strongGorjetaTotal.innerHTML = `R$ ${(conta * porcentagem / pessoas).toFixed(2)}`
 
       const strongTotal = document.querySelector(".total > strong")
-      strongTotal.innerHTML = `R$ ${((conta + (conta * porcentagem)) / pessoas).toFixed(2)}}`
+      strongTotal.innerHTML = `R$ ${((conta + (conta * porcentagem)) / pessoas).toFixed(2)}`
 
     }
 }
